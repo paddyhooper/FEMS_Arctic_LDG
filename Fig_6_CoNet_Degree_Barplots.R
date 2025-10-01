895f289f7a36081667e5563a64e4c654a3287bab
Paddy Hooper <83075067+paddyhooper@users.noreply.github.com>
2025-10-01 09:41:20 +0100

    Update and rename CoNet_Degree_Barplots.R to Fig_6_CoNet_Degree_Barplots.R

--- Fig_6_CoNet_Degree_Barplots.R
+++ Fig_6_CoNet_Degree_Barplots.R
@@ -1,32 +1,18 @@
 #Making bar graphs for CoNet analysis
-#21.12.22
 #Author: Patrick M. Hooper
-
-#I want to make a bar chart of abundance data, need to import the data as a .csv file and then plot in ggplot
-
-#Set working directory to the CoNet folder in my R output files
-setwd("C:/Users/pmh36/OneDrive - Natural History Museum/R/R_data/Co_Net")
-
+#Created: 21.12.22
 
 #Libraries
 library(ggplot2); packageVersion("ggplot2")
 
-#Going to need to specify the colours so they match :) 
-#I am going to add a colour column to the input file with the associated Hex Codes, like I did with the map file
-
-#Need to load my .csv files for the different graphs, these are in the following directory:
-#I made them in excel and exported the final count tables
-#C:\Users\pmh36\OneDrive - Natural History Museum\R\R_data\Co_Net\output_data
-
-
 #Network 1 - Prokaryotes and Protists and Fungi
-net_1_proks <- read.table("C:/Users/pmh36/OneDrive - Natural History Museum/R/R_data/Co_Net/output_data/16S_protist_fungi_proks.txt", header = TRUE)
-net_1_euks <- read.table("C:/Users/pmh36/OneDrive - Natural History Museum/R/R_data/Co_Net/output_data/16S_protist_fungi_euks.txt", header = TRUE)
+net_1_proks <- read.table("~/16S_protist_fungi_proks.txt", header = TRUE)
+net_1_euks <- read.table("~/16S_protist_fungi_euks.txt", header = TRUE)
 net_1_proks
 net_1_euks
 
 #network 2 - 18S only
-net_2_euks <- read.table("C:/Users/pmh36/OneDrive - Natural History Museum/R/R_data/Co_Net/output_data/18S_only_euks.txt", header = TRUE)
+net_2_euks <- read.table("~/18S_only_euks.txt", header = TRUE)
 net_2_euks
 
 #Plotting Network 1 Graphs
@@ -49,7 +35,6 @@ ggsave("net_1_euks_barplot.pdf", width = 297, height = 210, units = c("mm"))
 #Plotting network 2 graphs
 p_net_2_euks <- ggplot(data = net_2_euks, aes(x=Degrees, y=fct_reorder(Division, Degrees), fill=I(col)))+geom_bar(stat="identity", width = 0.6)+ theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 p_net_2_euks
-
 
 #use ggsave to save to your wd as a pdf and jpeg
 ggsave("net_2_euks_barplot.jpg", width = 210, height = 297, units = c("mm"))
