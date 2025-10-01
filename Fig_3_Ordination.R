e970b989f1aa9ed7d08d9256dd74b5e05d54a4a8
Paddy Hooper <83075067+paddyhooper@users.noreply.github.com>
2025-10-01 15:55:05 +0100

    Update Fig_3_Ordination.R

--- Fig_3_Ordination.R
+++ Fig_3_Ordination.R
@@ -131,7 +131,7 @@ sample_names(OTU_18S_ave)
 
 #3. Add averaged metadata
 #No KJB17bii in this file
-meta_16S <- read.table("~/GLOM_V4_16S_METADATA_FORMATTED_AVERAGE_UPDATE_17_8_22.txt") #NOTE THIS IS THE METADATA WITH THE READ COUNTS WITHOUT METAZOA AND PLANTS
+meta_16S <- read.table("~/16S_metadata_average.txt") #NOTE THIS IS THE METADATA WITH THE READ COUNTS WITHOUT METAZOA AND PLANTS
 head(meta_16S)
 rownames(meta_16S)
 nrow(meta_16S) #32
